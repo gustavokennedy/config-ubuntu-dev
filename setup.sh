b958cc73044832d153868fa9d886b4d4776521c2
Gustavo Kennedy Renkel <98914036+gustavokennedy@users.noreply.github.com>
2024-04-11 13:30:25 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,23 +15,6 @@ set -euo pipefail
 
 echo -e "${YELLOW} INICIANDO AMBIENTE ${RESET}"
 export DEBIAN_FRONTEND=noninteractive
-# Atualiza sistema
-echo "${RED} Atualizando sistema...${RESET}"
-sudo apt update --yes && sudo apt list --upgradable --yes && sudo apt upgrade --yes
-echo "${GREEN}----OK SISTEMA ATUALIZADO COM SUCESSO!${RESET}"
-# Limpa cache
-echo "${RED} Limpando cache do sistema...${RESET}"
-sudo apt autoremove --yes && sudo apt autoclean --yes && sudo apt clean --yes
-echo "${GREEN}----OK CACHE LIMPO COM SUCESSO!${RESET}"
-# Configura timezone
-echo "${RED} Configurando timezone do servidor...${RESET}"
-sudo timedatectl set-timezone "America/Sao_Paulo"
-sudo systemctl restart systemd-timesyncd.service
-echo "${GREEN}----OK TIMEZONE ATUALIZADO COM SUCESSO!${RESET}"
-# Instala pacotes
-echo "${RED} Instalando pacotes necessários..${RESET}."
-sudo apt-get install build-essential curl file git nginx --yes
-echo "${GREEN}----OK PACOTES INSTALADOS COM SUCESSO!${RESET}"
 # Instala NVM e Node
 echo "${RED}  Instalando NVM & NodeJS...${RESET}"
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh
