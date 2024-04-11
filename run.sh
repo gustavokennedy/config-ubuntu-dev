7500cf63ae2ed353d6ff6c978c192dc104f1aa6d
Gustavo Kennedy Renkel <98914036+gustavokennedy@users.noreply.github.com>
2024-04-11 15:04:32 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -104,7 +104,7 @@ echo "${GREEN}----OK ZSH INSTALADO COM SUCESSO!${RESET}"
 echo "${RED}  Instalando e configurando OhMyZsh e plugins no ZSH...${RESET}"
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 chmod +x install.sh
-./install.sh > /dev/null 2>&1
+./install.sh > /dev/null 2>&1 && exit
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
