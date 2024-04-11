62dd59cc27c8e54a9da345ec74d6c3ac48d8e9b8
Gustavo Kennedy Renkel <98914036+gustavokennedy@users.noreply.github.com>
2024-04-11 14:00:36 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -57,7 +57,7 @@ sudo systemctl reload nginx && sudo systemctl restart nginx
 echo "${GREEN}----OK NGINX REINICIADO COM SUCESSO!${RESET}"
 # Instala Docker
 echo "${RED}  Instalando Docker...${RESET}"
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common --yes
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
